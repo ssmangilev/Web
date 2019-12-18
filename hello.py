41adfb16c8c62c3bd4a20e86908ad9e32f44fea1
anberlin <ssmangilev@gmail.com>
2019-12-18 20:37:06 +0400

    renamed:    nginx.conf -> etc/nginx.conf 	modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 from urllib.parse import urlparse
 
-def wsgi_app(environ, start_response):
+def application(environ, start_response):
     if environ['REQUEST_METHOD']=='GET':
-        body = environ['QUERY_STRING'].replace('&','\n')
+        body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
     headers = [
             ('Content-type','text/plain')
             ]
     start_response(status,headers)
-    return [body]
+    return body
